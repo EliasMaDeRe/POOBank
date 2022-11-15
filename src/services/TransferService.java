12ef746836686ce6dca14e701067857e56f1b457
Carlos May <carlosmay220@gmail.com>
2022-11-15 12:17:28 -0600

    Update TransferService.java

--- src/services/TransferService.java
+++ src/services/TransferService.java
@@ -9,28 +9,38 @@ public class TransferService {
 	
 	Cuenta cuenta = new Cuenta();
 	
-    public WrapperResponse<TransferResponseDTO> transfer(TransferRequestDTO transfer){
+    public WrapperResponse<Transferencia> transfer(TransferRequestDTO transfer){
 
 		cuentaEmisora = cuenta.findAccountByAccountNumber(transfer.getNumeroDeCuentaEmisora());
 		cuentaReceptora = cuenta.findAccountByAccountNumber(transfer.getDestino());
+
+		boolean ok;
+		String mensaje;
+
+		Transferencia transferencia = new Transferencia(transfer.getNumeroDeCuenta(), transfer.getDestino(), transfer.getMonto(), transfer.getConcepto())
 
         if ((cuentaEmisora.isPresent())){ //Checa si existe la cuenta emisora
 			if ((cuentaReceptora.isPresent())){	//Checa si existe ela cuenta receptora
 				if(cuentaEmisora.getSaldo()>=transfer.getMonto()){	//Checa si tiene suficiente dinero la emisora					
 					cuentaEmisora.setSaldo(sustraerMonto(transfer.getMonto(), cuentaEmisora.getSaldo()));
 					cuentaReceptora.setSaldo(agregarMonto(transfer.getMonto(), cuentaReceptora.getSaldo()));
-					return new WrapperResponse<TransferResponseDTO>(true, "Transferencia exitosa", new transferResponseDTO(transfer.getNumeroDeCuentaEmisora(), transfer.getDestino(), transfer.getMonto(), transfer.getConcepto())); //Devuelve los valores nuevos
+
+					ok = true;
+					mensaje = "Transferencia exitosa";
 				} else {
-					return new WrapperResponse<TransferResponseDTO>(false, "No se pudo sustraer el monto de la cuenta emisora", null);
+					ok = false;
+					mensaje = "No se pudo sustraer el monto de la cuenta emisora";
 				}
 			} else {
-				return new WrapperResponse<TransferResponseDTO>(false, "La cuenta receptora no existe", null);
+				ok = false;
+				mensaje = "La cuenta receptora no existe";
 			}
         } else {
-            return new WrapperResponse<TransferResponseDTO>(false, "La cuenta emisora no existe", null);
+			ok = false;
+			mensaje = "La cuenta emisora no existe";
 		}
 
-		Transferencia transferencia = new Transferencia(transfer.getNumeroDeCuenta(), transfer.getDestino(), transfer.getMonto(), transfer.getConcepto())
+		return new WrapperResponse<Transferencia>(ok, mensaje, transferencia);
 
     }
 
