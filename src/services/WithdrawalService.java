324ef752dee198185e5bc46409c477f0242856dd
Carlos May <carlosmay220@gmail.com>
2022-11-15 12:06:25 -0600

    Update WithdrawalService.java

--- src/services/WithdrawalService.java
+++ src/services/WithdrawalService.java
@@ -1,20 +1,22 @@
 package services;
 
 import DTO.*;
+import models.Cuenta;
 import models.Retiro;
 import utilities.WrapperResponse;
 
 public class WithdrawalService {
 
-    private Retiro retiro = new Retiro();
+    private Cuenta cuenta = new Cuenta();
 
     public WithdrawalService() {}
 
     public WrapperResponse<WithdrawalResponseDTO> Withdrawal(WithdrawalRequestDTO retiro){
 
-        if(retiro.findUserByAccountNumber(retiro.getnumeroDeCuenta().isPresent())){
-            if(retiro.getSaldo() >= retiro.getMonto()){
-                retiro.getNumeroDeCuenta().setSaldo() = retiro.getSaldo() - retiro.getMonto();
+        if(cuenta.findUserByAccountNumber(retiro.getnumeroDeCuenta().isPresent())){
+            if(cuenta.getSaldo() >= retiro.getMonto()){
+
+                cuenta.getNumeroDeCuenta().setSaldo(cuenta.getSaldo()-retiro.getMonto());
                 return new WrapperResponse<WithdrawalResponseDTO>(true, "El retiro se realizó con éxito.", new WithdrawalResponseDTO(new TransactionDTO(retiro.getnumeroDeCuenta()), retiro.getSaldo(), retiro.getMonto()));
             }else{
                 return new WrapperResponse<WithdrawalResponseDTO>(false, "No hay saldo suficiente para lo que se desea retirar.", null);
