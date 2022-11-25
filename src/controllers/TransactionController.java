f674e4176fee0725ab798bd2599ee8d786340b9f
FernandoJoachin <a18000621@alumnos.uady.mx>
2022-11-25 00:42:03 -0600

    Update TransactionController y Documentacion

--- src/controllers/TransactionController.java
+++ src/controllers/TransactionController.java
@@ -1,6 +1,5 @@
 package controllers;
 import DTO.*;
-import models.Transferencia;
 import services.WithdrawalService;
 import services.DepositService;
 import services.TransferService;
@@ -20,27 +19,27 @@ public class TransactionController {
     /**
      * Método que se encarga de controlar el flujo de información entre la vista y el servicio retiro.
      * @param withdrawal Es un DTO del retiro.
-     * @return Devuelve un WrapperResponse del servicio retiro.
+     * @return Devuelve un booleano del servicio retiro.
      */
-    public WrapperResponse<WithdrawalResponseDTO> withdrawal(WithdrawalRequestDTO withdrawal) {
+    public WrapperResponse<Boolean> withdrawal(WithdrawalRequestDTO withdrawal) {
       return withdrawalService.Withdrawal(withdrawal);
 	  }
 
     /**
      * Método que se encarga de controlar el flujo de información entre la vista y el servicio depósito.
      * @param deposit Es un DTO del depósito.
-     * @return  Devuelve un WrapperResponse del servicio depósito.
+     * @return  Devuelve un booleano del servicio depósito.
      */
-    public WrapperResponse<DepositResponseDTO> depositService(DepositRequestDTO deposit) {
+    public WrapperResponse<Boolean> depositService(DepositRequestDTO deposit) {
       return depositService.Deposit(deposit);
 	  }
 
     /**
      * Método que se encarga de control el flujo de información entre la vista y el servicio transferencia.
      * @param transfer Es un DTO transferencia.
-     * @return Devuelve un WrapperResponse del servicio transferencia.
+     * @return Devuelve un booleano del servicio transferencia.
      */
-    public WrapperResponse<Transferencia> transfer(TransferRequestDTO transfer) {
+    public WrapperResponse<Boolean> transfer(TransferRequestDTO transfer) {
       return transferService.transfer(transfer);
 	  }
 }
